aee03f4df623427e270dadcb2b4b586397434aee
Jeff Stewart <pyearesqrd@gmail.com>
2011-09-20 01:11:25 -0700

    Both direction decoding along with velocity measurements.

--- tdd_decoder/decoder.v
+++ tdd_decoder/decoder.v
@@ -1,23 +1,59 @@
 //decoder.v
 
-module encoder(clock, reset, is_cw, is_ccw);
+module decoder(clock, reset, A, B, is_cw, is_ccw, omega);
    input clock;
    input reset;
+   input A;
+   input B;
 
    output is_cw;
    output is_ccw;
-
+   output [7:0] omega;
+   
    reg is_cw;
    reg is_ccw;
+   reg [7:0] omega;
+   
+   reg A_sync;
+   reg B_sync;
+   reg A_last;
+   reg B_last;
+
+   reg [7:0] cnt;
 
    always @(posedge clock)
    begin
       if(reset) begin
          is_cw <= 0;
          is_ccw <= 0;
+         omega <= 0;
+         
+         A_sync <= 0;
+         B_sync <= 0;
+         A_last <= 0;
+         B_last <= 0;
+         cnt <= 0;
       end
-      begin
-      
+      else begin
+         if (A_sync & !A_last) begin
+            if( B_sync) begin
+               is_cw  <= 0;
+               is_ccw <= 1;
+            end
+            else begin
+               is_cw  <= 1;
+               is_ccw <= 0;
+            end
+            omega <= cnt;
+            cnt <= 0;
+         end
+         else begin
+            cnt <= cnt + 1;
+         end
+         A_sync <= A;
+         B_sync <= B;
+         A_last <= A_sync;
+         B_last <= B_sync;
       end      
    end
 endmodule
--- tdd_decoder/decoder_tb.v
+++ tdd_decoder/decoder_tb.v
@@ -1,49 +1,156 @@
 //decoder_tb.v  
 
-module encoder_tb;
+module decoder_tb;
 reg clock;
 reg reset;
+reg A;
+reg B;
 
 wire is_cw;
 wire is_ccw;
+wire [7:0] omega;
 
-encoder dut(
+decoder dut(
       .clock(clock),
       .reset(reset),
+      .A(A),
+      .B(B),
       .is_cw(is_cw),
-      .is_ccw(is_ccw));
-
+      .is_ccw(is_ccw),
+      .omega(omega));
 
 reg tb_clock;
 
+parameter TB_CLOCK = 100;
+parameter DUT_CLOCK = 5;
+ 
+initial begin
+   $dumpfile("test.vcd");
+   $dumpvars(0,decoder_tb);
+end        
+
 initial begin
    tb_clock = 0;
-   forever #50 tb_clock = ~tb_clock;
+   forever #TB_CLOCK tb_clock = ~tb_clock;
 end
 
 initial begin
    clock = 0;
-   forever #9 clock = ~clock;
+   forever #DUT_CLOCK clock = ~clock;
 end
 
 initial begin
    reset_dut();
    #1 test_outputs_zero_on_reset();
+   #1 test_clockwise_input();
+   #1 test_counter_clockwise_input();
+   #1 test_calculate_velocity();
+   #1 test_switch_directions();
    #100 $finish;
 end
+
+task test_switch_directions;
+begin
+   reset_dut();
+   drive_inputs_counter_clockwise();
+   drive_inputs_counter_clockwise();
+   drive_inputs_clockwise();
+   drive_inputs_clockwise();
+   if( !is_cw || is_ccw) begin
+      $display("Test Failed: switch_directions()");
+      $finish;
+   end
+end
+endtask   
+
+task test_calculate_velocity;
+integer compare;
+begin
+   compare = (4 * 2 * TB_CLOCK) / (2 * DUT_CLOCK) - 1;
+   reset_dut();
+   drive_inputs_counter_clockwise();
+   drive_inputs_counter_clockwise();
+   drive_inputs_counter_clockwise();
+   drive_inputs_counter_clockwise();
+   if(omega <= compare - 1 || omega >= compare + 1) begin
+      $display("Test Failed: calculate_velocity()");
+      $finish;
+   end
+end
+endtask
+
+task test_counter_clockwise_input;
+begin
+   reset_dut();
+   drive_inputs_counter_clockwise();
+   drive_inputs_counter_clockwise();
+   if( is_cw || !is_ccw) begin
+      $display("Test Failed: counter_clockwise_input()");
+      $finish;
+   end
+end
+endtask   
+
+task test_clockwise_input;
+begin
+   reset_dut();
+   drive_inputs_clockwise();
+   drive_inputs_clockwise();
+   if( !is_cw || is_ccw) begin
+      $display("Test Failed: clockwise_input()");
+      $finish;
+   end
+end
+endtask
 
 task test_outputs_zero_on_reset;
 begin
    reset_dut();
-   if ( is_cw == 1 || is_ccw == 1) begin
+   if ( is_cw !== 0 || is_ccw !== 0 ) begin
       $display("Test Failed: outputs_zero_on_reset()");
       $finish;
    end
 end
 endtask
 
+task drive_inputs_counter_clockwise;
+begin
+   @(posedge tb_clock);
+   A = 0;
+   B = 0;
+   @(posedge tb_clock);
+   A = 0;
+   B = 1;
+   @(posedge tb_clock);
+   A = 1;
+   B = 1;
+   @(posedge tb_clock);
+   A = 1;
+   B = 0;
+end
+endtask
+
+task drive_inputs_clockwise;
+begin
+   @(posedge tb_clock);
+   A = 0;
+   B = 0;
+   @(posedge tb_clock);
+   A = 1;
+   B = 0;
+   @(posedge tb_clock);
+   A = 1;
+   B = 1;
+   @(posedge tb_clock);
+   A = 0;
+   B = 1;
+end
+endtask
+
 task reset_dut;
 begin
+   A = 0;
+   B = 0;
    reset = 0;
    @(posedge tb_clock);
    reset = 1;
